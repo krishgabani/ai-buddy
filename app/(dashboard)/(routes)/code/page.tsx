90c82873b6ed35261f1e4037ebd7d8d92ec9b657
Krish-Gabani <krishgabani7@gmail.com>
2023-08-13 13:26:03 +0530

    Code generation completed

---
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -0,0 +1,162 @@
+"use client";
+
+import axios from "axios";
+import * as z from "zod"; // TypeScript-first schema validation library
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Code } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod"; // from shadcn
+import { ChatCompletionRequestMessage } from "openai";
+import ReactMarkdown from "react-markdown";
+
+import Heading from "@/components/Heading";
+import Empty from "@/components/Empty";
+import Loader from "@/components/Loader";
+import UserAvatar from "@/components/User-Avatar";
+import BotAvatar from "@/components/Bot-Avatar";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { formSchema } from "./constants";
+import { cn } from "@/lib/utils";
+
+const CodePage = () => {
+  const router = useRouter();
+
+  // Add every question & answer to this array
+  // on every request to API - pass current & old messages
+  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([
+    { role: "user", content: "who invented atom bomb?" },
+    {
+      role: "assistant",
+      content:
+        "The atomic bomb was invented by a team of scientists, led by J. Robert Oppenheimer, as part of the Manhattan Project during World War II. While Oppenheimer is often credited with the development of the atomic bomb, it was a collaborative effort involving many scientists, including Enrico Fermi, Leo Szilard, and Edward Teller, among others.",
+    },
+    { role: "user", content: "when it used?" },
+  ]);
+
+  // React-hook
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      // currently typed message
+      const userMessage: ChatCompletionRequestMessage = {
+        role: "user",
+        content: values.prompt,
+      };
+
+      // add previous messages
+      const newMessages = [...messages, userMessage];
+
+      const response = await axios.post("/api/code", {
+        messages: newMessages,
+      });
+      console.log(response);
+
+      // update previous messages
+      setMessages((current) => [...current, userMessage, response.data]);
+
+      form.reset();
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Code Generation"
+        description="Generate code using descriptive text.."
+        icon={Code}
+        iconColor="text-green-700"
+        bgColor="text-green-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Simple toggle button using react hooks."
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loader />
+            </div>
+          )}
+          {messages.length === 0 && !isLoading && (
+            <Empty label={"No conversation started."} />
+          )}
+          <div className="flex flex-col-reverse gap-y-3">
+            {messages.map((message) => (
+              <div
+                key={message.content}
+                className={cn(
+                  "p-6 w-full flex items-center gap-x-8 rounded-lg",
+                  message.role === "user"
+                    ? "bg-white border border-black/10"
+                    : "bg-muted"
+                )}
+              >
+                {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+
+                <ReactMarkdown
+                  components={{
+                    pre: ({ node, ...props }) => (
+                      <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                        <pre {...props} />
+                      </div>
+                    ),
+                    code: ({ node, ...props }) => (
+                      <code className="bg-black/10 p-1 rounded-lg" {...props} />
+                    ),
+                  }}
+                  className="tetx-sm overflow-hidden"
+                >
+                  {message.content || ""}
+                </ReactMarkdown>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CodePage;
